10f01de43c84f6e0107833bbc3bcb2b2db2971fc
Jvnyor <=>
2022-12-26 22:58:41 -0300

    first commit

---
+++ src/main/java/com/Jvnyor/server/enumeration/Status.java
@@ -0,0 +1,16 @@
+package com.Jvnyor.server.enumeration;
+
+public enum Status {
+	SERVER_UP("SERVER_UP"),
+	SERVER_DOWN("SERVER_DOWN");
+	
+	private final String status;
+	
+	Status(String status) {
+		this.status = status;
+	}
+	
+	private String getStatus() {
+		return status;
+	}
+}
---
+++ src/main/java/com/Jvnyor/server/repo/ServerRepo.java
@@ -0,0 +1,5 @@
+package com.Jvnyor.server.repo;
+
+public interface ServerRepo {
+
+}
